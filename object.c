aadc0c84764ed1bc9c7c175669b6edc65ca6bb3e
Almaz Shinbay <shinbay@Almazs-MacBook-Pro-2.local>
2022-09-14 11:37:10 +0600

    added string concatenation

--- object.c
+++ object.c
@@ -22,9 +22,21 @@ static ObjString* allocateString(char* chars, int length) {
 	return string;
 }
 
+ObjString* takeString(char* chars, int length) {
+	return allocateString(chars, length);
+}
+
 ObjString* copyString(const char* chars, int length) {
 	char* heapChars = ALLOCATE(char, length + 1);
 	memcpy(heapChars, chars, length);
 	heapChars[length] = '\0';
 	return allocateString(heapChars, length);
+}
+
+void printObject(Value value) {
+	switch (OBJ_TYPE(value)) {
+		case OBJ_STRING:
+			printf("%s", AS_CSTRING(value));
+			break;
+	}
 }
