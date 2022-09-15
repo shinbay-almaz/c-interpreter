3d409372fe441facdde3adf9c0d1aa14fe71faaa
Almaz Shinbay <shinbay@Almazs-MacBook-Pro-2.local>
2022-09-15 14:14:20 +0600

    added freeing objects

--- object.c
+++ object.c
@@ -12,6 +12,9 @@
 static Obj* allocateObj(size_t size, ObjType type) {
 	Obj* object = (Obj*)reallocate(NULL, 0, size);
 	object->type = type;
+
+	object->next = vm.objects;
+	vm.objects = object;
 	return object;
 }
 
