18d41bbc146fd00f15b22918cf1819e2838cdbc7
Almaz Shinbay <shinbay@Almazs-MacBook-Pro-2.local>
2022-09-29 12:37:28 +0600

    added local variables

--- compiler.c
+++ compiler.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "common.h"
 #include "compiler.h"
@@ -161,6 +162,13 @@ static void beginScope() {
 
 static void endScope() {
 	current->scopeDepth--;
+
+	while (current->localCount > 0 && 
+		   current->locals[current->localCount - 1].depth >
+				current->scopeDepth) {
+		emitByte(OP_POP);
+		current->localCount--;
+	}
 }
 
 static void expression();
@@ -174,12 +182,72 @@ static uint8_t identifierConstant(Token* name) {
 										   name->length)));
 }
 
+static bool identifiersEqual(Token* a, Token* b) {
+	if (a->length != b->length) return false;
+	return memcmp(a->start, b->start, a->length) == 0;
+}
+
+static int resolveLocal(Compiler* compiler, Token* name) {
+	for (int i = compiler->localCount - 1; i >= 0; i--) {
+		Local* local = &compiler->locals[i];
+		if (identifiersEqual(name, &local->name)) {
+			if (local->depth == -1) {
+				error("Can't read local variable in its own initializer.");
+			}
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+static void addLocal(Token name) {
+	if (current->localCount == UINT8_COUNT) {
+		error("Too many local variables in function.");
+		return;
+	}
+
+	Local* local = &current->locals[current->localCount++];
+	local->name = name;
+	local->depth = -1; 
+}
+
+static void declareVariable() {
+	if (current->scopeDepth == 0) return;
+
+	Token* name = &parser.previous;
+	for (int i = current->localCount - 1; i >= 0; i--) {
+		Local* local = &current->locals[i];
+		if (local->depth != -1 && local->depth < current->scopeDepth) {
+			break;
+		}
+
+		if (identifiersEqual(name, &local->name)) {
+			error("Already a variable with this name in this scope.");
+		}
+	}
+	addLocal(*name);
+}
+
 static uint8_t parseVariable(const char* errorMessage) {
 	consume(TOKEN_IDENTIFIER, errorMessage);
+
+	declareVariable();
+	if (current->scopeDepth > 0) return 0;
+
 	return identifierConstant(&parser.previous);
 }
 
+static void markInitialized() {
+	current->locals[current->localCount - 1].depth = current->scopeDepth;
+}
+
 static void defineVariable(uint8_t global) {
+	if (current->scopeDepth > 0) {
+		markInitialized();
+		return;
+	}
+
 	emitBytes(OP_DEFINE_GLOBAL, global);
 }
 
@@ -229,12 +297,23 @@ static void string(bool canAssign) {
 }
 
 static void namedVariable(Token name, bool canAssign) {
-	uint8_t arg = identifierConstant(&name);
+	uint8_t getOp, setOp;
+	int arg = resolveLocal(current, &name);
+	if (arg != -1) {
+		getOp = OP_GET_LOCAL;
+		setOp = OP_SET_LOCAL;
+	} else {
+		arg = identifierConstant(&name);
+		getOp = OP_GET_GLOBAL;
+		setOp = OP_SET_GLOBAL;
+	}
+
 	if (canAssign && match(TOKEN_EQUAL)) {
 		expression();
-		emitBytes(OP_SET_GLOBAL, arg);
+
+		emitBytes(setOp, (uint8_t)arg);
 	} else {
-		emitBytes(OP_GET_GLOBAL, arg);
+		emitBytes(getOp, (uint8_t)arg);
 	}
 }
 
--- debug.c
+++ debug.c
@@ -36,6 +36,12 @@ static int simpleInstruction(const char* name, int offset) {
 	return offset + 1;
 }
 
+static int byteInstruction(const char* name, Chunk* chunk, int offset) {
+	uint8_t slot = chunk->code[offset + 1];
+	printf("%-16s %4d\n", name, slot);
+	return offset + 2;
+}
+
 int getLine(Chunk* chunk, int offset) {
 	int index = offset;
 	for (int i = 0; i < chunk->lineCount;) {
@@ -75,15 +81,16 @@ int disassembleInstruction(Chunk* chunk, int offset) {
 			return simpleInstruction("OP_FALSE", offset);
 		case OP_POP:
 			return simpleInstruction("OP_POP", offset);
+		case OP_GET_LOCAL:
+			return byteInstruction("OP_GET_LOCAL", chunk, offset);
 		case OP_GET_GLOBAL:
-			return constantInstruction("OP_GET_GLOBAL", chunk, 
-									   offset);
+			return constantInstruction("OP_GET_GLOBAL", chunk, offset);
 		case OP_DEFINE_GLOBAL:
-			return constantInstruction("OP_DEFINE_GLOBAL", chunk, 
-									   offset);
+			return constantInstruction("OP_DEFINE_GLOBAL", chunk, offset);
+		case OP_SET_LOCAL:
+			return byteInstruction("OP_SET_LOCAL", chunk, offset);
 		case OP_SET_GLOBAL:
-			return constantInstruction("OP_SET_GLOBAL", chunk,
-									   offset);
+			return constantInstruction("OP_SET_GLOBAL", chunk, offset);
 		case OP_EQUAL:
 			return simpleInstruction("OP_EQUAL", offset);
 		case OP_GREATER:
